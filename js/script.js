29245baea9e28f4d34c73b3697aa2d792088c62d
mahsa <mahmoudmaha@hotmail.com>
2019-02-15 15:33:52 +0100

    create html,ts,js,css files and folders

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function;
+showthetext();
+{
+    var randomtext = "Hello World!";
+    /* in Typescript now you can pick the type of the value for the variable that you create */
+    document.getElementById("result").innerHTML = randomtext;
+}
---
+++ script.ts
@@ -0,0 +1,5 @@
+function showthetext(){
+     var randomtext :string = "Hello World!";
+/* in Typescript now you can pick the type of the value for the variable that you create */
+     document.getElementById("result").innerHTML = randomtext;
+}
