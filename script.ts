eb044ca44833057c54956b16cf60cb3e047822cd
mahsa <mahmoudmaha@hotmail.com>
2019-02-15 20:13:56 +0100

    delete test and add the links

--- script.ts
+++
@@ -1,5 +0,0 @@
-function showthetext(){
-     var randomtext :string = "Hello World!";
-/* in Typescript now you can pick the type of the value for the variable that you create */
-     document.getElementById("result").innerHTML = randomtext;
-}
